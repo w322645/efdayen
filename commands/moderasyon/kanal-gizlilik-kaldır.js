3279e1b80ba416be4bb6a2b35424fe5cc6fc1dd0
Glitch (blush-lead-argument) <none>
2022-04-12 11:10:48 +0000

    🍠👩‍👩‍👦 Checkpoint ./commands/moderasyon/kanal-gizle.js:33847415/1231 ./commands/moderasyon/kanal-gizlilik-kaldır.js:33847415/966 ./commands/yardÄ±m/menÃ¼yetkili2.js:33847415/360

---
+++ commands/moderasyon/kanal-gizlilik-kaldır.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js'); //modüller
+
+exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_CHANNELS"))
+return message.channel.send("Bu komutu kullanabilmek için **Kanalları Yönet**yetkisine sahip olmalısın");
+message.channel.send("Kanal Üyelere Görünür Hale Getirildi.")
+  let every = message.guild.roles.cache.find(r => r.name === "@everyone");
+  message.channel.createOverwrite(every, {
+    VIEW_CHANNEL: true
+  });
+}
+exports.conf = {
+    enabled: true, //kullanıma açık mı değil mi
+    guildOnly: false, //dmde kullanıma açık mı değil mi
+    aliases: ['kanal-görünürlük-aç'], //kısayollar
+    permLevel: 0 //perm level mainde karşıliklar yazar
+  };
+ 
+  exports.help = {
+    name: "kanal-göster", //komutu çalıştıracak olan kelime
+    description: "",//açıklama (isteğe bağlı)
+    usage: ""//kullanım (isteğe bağlı)
+  }; 
