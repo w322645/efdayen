577efa33d12f8ba47258b151ef139d4601840d4d
Glitch (blush-lead-argument) <none>
2022-04-14 14:53:35 +0000

    💂🏎 Checkpoint ./commands/yardÄ±m/anayardÄ±m.js:33847415/308

--- commands/yardÄ±m/anayardÄ±m.js
+++ commands/yardÄ±m/anayardÄ±m.js
@@ -12,6 +12,7 @@ exports.run = async (client, message, args) => {
     var prefix = "?";
   }
   };
+  var sikiş = db.fetch(`sahıp_${message.author.id}`)
   const sgmk = new Discord.MessageEmbed()
     .setColor("#36393f")
     .setAuthor(
@@ -27,7 +28,7 @@ exports.run = async (client, message, args) => {
     )
     .setTimestamp()
     .setDescription(
-      `\`\`\`bash\n"EFDA Menüler"\n\`\`\`\`\`\`asciidoc\n[Prefixi Değiştirmek için ${prefix}prefix]\n\`\`\`\n\`${prefix}yenilikler : Yenilikler Menüsüne Gidersiniz\`  \`${prefix}efday\`\n\n\`${prefix}destek-sunucu : Destek Sunucusuna gidersiniz\`  \`${prefix}d\`\n\n\`${prefix}kayıt-menü : Kayıt Menüsüne Gidersiniz\`  \`${prefix}kayıtm\`\n\n\`${prefix}yetkili : Yetkili Menüsüne Gidersiniz\`  \`${prefix}y\`\n\n\`${prefix}yetkili2 : Yetkili (2) Menüsüne Gidersiniz\`  \`${prefix}y2\`\n\n\`${prefix}logo : Logo Menüye Gidersiniz\`  \`${prefix}lg\`\n\n\`${prefix}menü : Menüye Gidersiniz\`  \`${prefix}m\`\n\n\`${prefix}menü2 : 2. Menüye Gidersiniz\`  \`${prefix}m2\`\n\n\`${prefix}menü3 : 3. Menüye Gidersiniz\`  \`${prefix}m3\`\n\n**🍭SORUN KOMUTLARI**\`\`\`fix\n${prefix}efda-öneri : Bota Hakkında Öneri Vermenizi Sağlar\n\n${prefix}bugbildir : Bot daki Bugları Bildirmenize Yarar\n\n${prefix}şikayet : Botu Şikayet Etmenize Yarar\`\`\``
+      `\`\`\`bash\n"EFDA Menüler"\n\`\`\`\`\`\`asciidoc\n[Prefixi Değiştirmek için ${prefix}prefix]\n\nSahibimin Durumu "${sikiş}"\n\n\`\`\`\n\`${prefix}yenilikler : Yenilikler Menüsüne Gidersiniz\`  \`${prefix}efday\`\n\n\`${prefix}destek-sunucu : Destek Sunucusuna gidersiniz\`  \`${prefix}d\`\n\n\`${prefix}kayıt-menü : Kayıt Menüsüne Gidersiniz\`  \`${prefix}kayıtm\`\n\n\`${prefix}yetkili : Yetkili Menüsüne Gidersiniz\`  \`${prefix}y\`\n\n\`${prefix}yetkili2 : Yetkili (2) Menüsüne Gidersiniz\`  \`${prefix}y2\`\n\n\`${prefix}logo : Logo Menüye Gidersiniz\`  \`${prefix}lg\`\n\n\`${prefix}menü : Menüye Gidersiniz\`  \`${prefix}m\`\n\n\`${prefix}menü2 : 2. Menüye Gidersiniz\`  \`${prefix}m2\`\n\n\`${prefix}menü3 : 3. Menüye Gidersiniz\`  \`${prefix}m3\`\n\n**🍭SORUN KOMUTLARI**\`\`\`fix\n${prefix}efda-öneri : Bota Hakkında Öneri Vermenizi Sağlar\n\n${prefix}bugbildir : Bot daki Bugları Bildirmenize Yarar\n\n${prefix}şikayet : Botu Şikayet Etmenize Yarar\`\`\``
     )
     .setImage("https://cdn.glitch.com/478cc1e8-9c56-49a4-b910-3247fb1894aa%2Fyeni2.png?v=1621792931950");
   message.channel.send(sgmk);
